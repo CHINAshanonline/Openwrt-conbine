720a975e6399dbb15d37505e1e25cd7f97c4b40a
CHINAshanonline <44047819+CHINAshanonline@users.noreply.github.com>
2020-04-26 10:52:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
