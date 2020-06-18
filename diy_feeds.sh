079cd2c32b08ebefa38dfb5406b09cd0e73c4cf4
CHINAshanonline <44047819+CHINAshanonline@users.noreply.github.com>
2020-06-18 12:44:19 +0800

    Update diy_feeds.sh

--- diy_feeds.sh
+++ diy_feeds.sh
@@ -26,6 +26,6 @@ echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull origin master
 git branch --set-upstream-to=origin/master master
 
-pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+# pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
